ec1e321407e98d1a450fee5254bd88aa99549d4d
Leonard Wall <12565112+leonardwall@users.noreply.github.com>
2022-10-26 18:58:54 -0400

    Create dns.bat

---
+++ 07. Cyber Infrastructure & Technology/dns.bat
@@ -0,0 +1,15 @@
+REM Turns off displaying to out put of commands
+@echo off
+
+
+REM Requests the user to input a target domain and saves it as the value of the %domain% variable
+set /p domain= enter domain to preform recon:
+
+REM Performes a txt lookup to the target domain,outputs the results to results.txt
+nslookup -type=txt %domain% >> results.txt
+
+REM Performes a mx lookup to the target domain,outputs the results to results.txt
+nslookup -type=mx %domain% >> results.txt
+
+REM Performes a ns lookup to the target domain,outputs the results to results.txt
+nslookup -type=ns %domain% >> results.txt
